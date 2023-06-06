b93214d2bc3434b92326aa870062171c45c05b2a
s1ntaxe770r <cyberdev01@protonmail.com>
2023-06-06 19:40:36 +0100

    add about page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Homepage from '../components/Homepage';
 import Navpage from '../components/Navpage';
+import About from '../components/About';
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <div className=" ">
         <Navpage />
         <Homepage />
+        <About/>
       </div>
     </div>
   );
