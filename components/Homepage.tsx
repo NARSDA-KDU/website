5434aa3c242828afd1e7a0a7a3abb812c69f108f
Emmanuel Tokula <emmanueltokula03@gmail.com>
2023-06-09 11:44:34 +0100

    updates on the components pages

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
 
 import { motion } from 'framer-motion'
 
@@ -7,15 +8,21 @@ type props = {
 }
  
 function Homepage  ( {}:props) {
+
+  const [text] = useTypewriter({
+    words: ['Do  ', 'you', ' Love', 'space!'],
+    loop: 3,
+    onLoopDone: () => console.log(`loop completed after 3 runs.`)
+  })
  
-   
+        
 
 
     
 
 
   return (
-    <section  className="items-center  justify-between mx-auto sticky px-8 h-screen  flex bg-black " >
+    <section  className="sticky items-center justify-between h-screen px-8 mx-auto xl:flex " >
         {/* Used frame motion for the the transition */}
         <  motion.div className='space-y-3' 
           initial={{
@@ -35,21 +42,23 @@ function Homepage  ( {}:props) {
       duration:1.5,
     }} >
 
-            <h1 className="space-y-3 font-semibold tracking-wide text-5xl lg:text-6xl xl:text-5xl" >
-                <span className="block  text-white  bg-clip-text text-transparent  " > Welcome to Nasrda  </span>
-                <span className="block font-bold md:text-[60px] text-white font-serif " >Club</span>
+            <h1 className="space-y-3 text-4xl font-semibold tracking-wide lg:text-6xl xl:text-7xl" >
+                <span className="block text-transparent text-white uppercase bg-clip-text " > Welcome to NASRDA  </span>
+                <span className="block font-bold md:text-[60px] text-white font-serif " >{text}</span>
                 <p  className="text-[20px] text-[#fff] " >something about us </p>
                 <p  className="text-[20px] text-[#fff] " >something about us </p>
                 <p  className="text-[20px] text-[#fff] " >something about us </p>
             </h1>
-            <div className="space-x-8 " >
+            <div className="space-x-8 text-white " >
                 {/* <Button title="Order Now" />  */}
-                <a className="Link" > Learn More </a>
+                <a className=" inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] 
+                focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]
+                 dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]" > Learn More </a>
             </div>
         </motion.div>
      
       
-        <motion.div className="relative mx-5 hidden h-[400px] w-[400px] transition-all  duration-500 md:inline lg:h-[650px] lg:w-[600px]"
+        <motion.div className="relative mx-5 h-[300px] w-[300px]  lg:h-[400px] lg:w-[400px] transition-all  duration-500 md:inline xl:h-[650px] xl:w-[600px]"
          initial={{
           x:500,
          opacity:0,
@@ -72,7 +81,7 @@ function Homepage  ( {}:props) {
      
        
        
-        <div  className=" p-9 "> <img src='/img/bg-1.jpg'  width={500} height={1000}  /> </div>
+        <div  className=" p-9"> <img src='/img/bg-1.jpg'  width={500} height={1000}  /> </div>
       
        
         
--- components/Navpage.tsx
+++ components/Navpage.tsx
@@ -1,11 +1,55 @@
 import React from 'react'
+import { SocialIcon } from 'react-social-icons';
+import { motion } from "framer-motion"
 
-function Navpage() {
+type props = {}
+
+function Navpage  ({}:props) {
   return (
-    <div>
-        navbar
+    //  Used frame motion for the the transition
+    <header  className="sticky top-0 z-20 flex items-start justify-between xl:items-center " >
+    <  motion.div 
+    initial={{
+      x:-500,
+     opacity:0,
+      scale:0.5 
+
+    }}
+
+    animate={{
+      x: - 0,
+      opacity:1,
+      scale:1
+    }}
+
+    transition={{
+      duration:1.5,
+    }}
+    >
+{/* made use of react icons */}
+      <SocialIcon url="https://github.com/NARSDA-KDU" fgColor="gray" bgColor="transparent" />
+
+      <SocialIcon url="https://twitter.com/s1ntaxe770r" fgColor="gray" bgColor="transparent" />
+
+      <SocialIcon url="https://www.youtube.com/sonnysangha" fgColor="gray" bgColor="transparent" />
+    </motion.div>
+
+    < div 
+    
+
+    
+    className="flex flex-row p-2 mx-10 text-gray-300 cursor-pointer" >
+    
+      <div classname="mx-4" ><img src='/img/kdu.png'  width={50} height={50}  /></div>
+      <div><img src='/img/nasrda.png'  width={50} height={50}  /></div>
+
+     
       
+  
+
+    
     </div>
+    </header>
   )
 }
 
