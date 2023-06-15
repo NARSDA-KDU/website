e5f94fc1b0fc9ed4a44a463ad6391b7e751ca6d9
tedph-cyber <tedph6@gmail.com>
2023-06-15 20:16:58 +0100

    modified:   components/Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ import { AiFillTwitterCircle, AiFillLinkedin, AiFillYoutube } from "react-icons/
 
 function Navbar() {
   return (
-    <section className='sticky top-0 py-4 z-20 max-h-14 bg-black grid grid-rows-2'>
+    <section className='sticky top-0 pt-4 z-20 bg-black grid grid-rows-2 h-2'>
         {/* unnecessary  logo since my "partner* alreaady did it/}
       {/* <div className=' p-2 flex w-screen isolate justify-stretch'>
         <Image 
@@ -24,7 +24,7 @@ function Navbar() {
       </div> */}
       <nav className=' bg-white bg-opacity-50 justify-around py-1 px-2 rounded-full h-12 w-2/3  mx-auto border-slate-500'>
         <ul className='justify-evenly flex mx-auto items-center '>
-          <li className='text-1xl hover:text-2xl rounded ring-2 ring-blue-500 text-white transition ease-in-out delay-100 hover:translate-y-1 hover:scale-110 hover:ring-indigo-500 duration-200'>
+          <li className='text-1xl hover:text-2xl rounded text-white transition ease-in-out delay-100 hover:translate-y-1 hover:scale-110 hover:ring-indigo-500 duration-200'>
             <Link href="#">Home</Link>
           </li>
           <li className='text-1xl hover:text-2xl text-white transition ease-in-out delay-100'>
